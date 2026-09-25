a7431441f30050a7df386bcda74edd9503a378e8
hibasar2026-boop <3.30502796e+08+hibasar2026-boop@users.noreply.github.com>
2026-09-25 12:55:07 +0000

    Fix GitHub Pages asset base path

--- assets/football/assets/abstractAudioBus-1UKG0nVg.js
+++
@@ -1,1 +0,0 @@
-import{ad as o}from"./index-BI2pSn_A.js";class u extends o{constructor(s,t){super(s,t,3)}}export{u as A};
---
+++ assets/football/assets/abstractAudioBus-DptTXiEw.js
@@ -0,0 +1,1 @@
+import{ad as o}from"./index-eSB3FQ0F.js";class u extends o{constructor(s,t){super(s,t,3)}}export{u as A};
