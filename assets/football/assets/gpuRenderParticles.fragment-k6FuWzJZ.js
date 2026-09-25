71eb9aa4de2058d1555fdfabc9d00835be931eb2
hibasar2026-boop <3.30502796e+08+hibasar2026-boop@users.noreply.github.com>
2026-09-25 12:49:54 +0000

    Add horizontal realistic 11v11 football game

---
+++ assets/football/assets/gpuRenderParticles.fragment-k6FuWzJZ.js
@@ -0,0 +1,66 @@
+import{k as e,o as i,p as n,l as t,aC as l,m as f,z as s,aD as S,n as c,u as d,r as m,s as g,v as P,w as p}from"./index-BI2pSn_A.js";const a="gpuRenderParticlesPixelShader",o=`var diffuseSamplerSampler: sampler;var diffuseSampler: texture_2d<f32>;varying vUV: vec2f;varying vColor: vec4f;
+#ifdef PREPASS
+uniform geometryZeroAlphaDiscard: f32;
+#ifdef PREPASS_POSITION
+varying vGeometryPositionW: vec3f;
+#endif
+#ifdef PREPASS_WORLD_NORMAL
+varying vGeometryNormalW: vec3f;
+#endif
+#ifdef PREPASS_NORMAL
+varying vGeometryNormalV: vec3f;
+#endif
+#endif
+#define PREPASS_VELOCITY_ZERO
+#include<prePassDeclaration>[SCENE_MRT_COUNT]
+#include<clipPlaneFragmentDeclaration>
+#include<imageProcessingDeclaration>
+#include<logDepthDeclaration>
+#include<helperFunctions>
+#include<imageProcessingFunctions>
+#include<fogFragmentDeclaration>
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#include<clipPlaneFragment>
+let textureColor: vec4f=textureSample(diffuseSampler,diffuseSamplerSampler,input.vUV);var baseColor: vec4f=textureColor*input.vColor;
+#ifdef PREPASS
+let geometryAlbedo: vec3f=toLinearSpaceVec3(baseColor.rgb);
+#endif
+#ifdef BLENDMULTIPLYMODE
+let alpha: f32=input.vColor.a*textureColor.a;baseColor=vec4f(baseColor.rgb*alpha+vec3f(1.0)*(1.0-alpha),baseColor.a);
+#endif
+#include<logDepthFragment>
+#include<fogFragment>(color,baseColor)
+#ifdef IMAGEPROCESSINGPOSTPROCESS
+baseColor=vec4f(toLinearSpaceVec3(baseColor.rgb),baseColor.a);
+#else
+#ifdef IMAGEPROCESSING
+baseColor=vec4f(toLinearSpaceVec3(baseColor.rgb),baseColor.a);baseColor=applyImageProcessing(baseColor);
+#endif
+#endif
+#ifdef PREPASS
+let geometryColor: vec4f=baseColor;if (geometryColor.a<=0.0 && uniforms.geometryZeroAlphaDiscard>0.0) {discard;}
+#ifdef PREPASS_POSITION
+let geometryPositionW: vec3f=input.vGeometryPositionW;
+#endif
+#ifdef PREPASS_LOCAL_POSITION
+let geometryPositionL: vec3f=input.vPosition;
+#endif
+#ifdef PREPASS_DEPTH
+let geometryViewDepth: f32=input.vViewPos.z;
+#endif
+#ifdef PREPASS_NORMALIZED_VIEW_DEPTH
+let geometryNormalizedViewDepth: f32=input.vNormViewDepth;
+#endif
+#ifdef PREPASS_NORMAL
+let geometryNormalV: vec3f=normalize(input.vGeometryNormalV);
+#endif
+#ifdef PREPASS_WORLD_NORMAL
+let geometryNormalW: vec3f=normalize(input.vGeometryNormalW);
+#endif
+#include<geometryRenderingFragment>
+#else
+fragmentOutputs.color=baseColor;
+#endif
+}
+`;e.ShadersStoreWGSL[a]||(e.ShadersStoreWGSL[a]=o);const u=[i,n,t,l,f,s,S,c,d,m,g,P,p];for(const r of u)e.IncludesShadersStoreWGSL[r.name]||(e.IncludesShadersStoreWGSL[r.name]=r.shader);const L={name:a,shader:o};export{L as gpuRenderParticlesPixelShaderWGSL};
